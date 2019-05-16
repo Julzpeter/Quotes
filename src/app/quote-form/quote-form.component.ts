2740a39131ecba5df2b8694fb0a3725ec93c9661
Juliet <chepngetichjuliet@gmail.com>
2019-05-16 20:04:00 +0300

    corrects the indentation

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
-import { Quote} from '../quote'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote} from '../quote';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -8,12 +8,12 @@ import { Quote} from '../quote';
 export class QuoteComponent implements OnInit {
   quotes = []
 
-  deleteQuote(isComplete,index){
-    if (isComplete){
-      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
 
-      if(toDelete){
-        this.quotes.splice(index,1)
+      if (toDelete) {
+        this.quotes.splice(index, 1)
       }
     }
   }
